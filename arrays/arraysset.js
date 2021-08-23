9a4a726e6d1d6e5c3af5d78d98dcdb1b34380e75
shahlo-alavova <64728278+shahlo-alavova@users.noreply.github.com>
2021-08-23 12:09:57 +0600

    Add files via upload

---
+++ arrays/arraysset.js
@@ -0,0 +1,49 @@
+// printReverse
+
+// var myArray=[];
+// for(var i=1; i<=5; i++){
+// var newItem = prompt("Gimme item");
+// myArray.push(newItem);
+// }
+// for(var i=1;i<=myArray.length; i++){
+// 	console.log (myArray[myArray.length-i]);
+// }
+
+// isUniform
+
+// var myArray_2=[];
+// for(var i=1; i<=4; i++){
+// var newItem = prompt("Gimme item");
+// myArray_2.push(newItem);
+// // }
+// for(var i = 1; i< myArray_2.length; i++){
+// // if(myArray_2[0] === myArray[i]){
+// // console.log("true")};
+
+// sumArray
+
+
+// function sumArray(myArray_3){
+// 	var total = 0;
+// 	myArray_3.forEach(function(numbers){
+// 		total +=numbers;
+// 	});
+// 	return total;
+// }
+
+
+// max()
+// var myArray_4=[];
+// for(var i=1; i<=4; i++){
+// var newItem = prompt("Gimme item");
+// myArray_4.push(newItem);
+// }
+
+// var max = myArray_4[0];
+// for(i=1; i<=myArray_4.length; i++){
+// if (myArray_4[i]> max){
+// 	var max = myArray_4[i]}
+// }
+// console.log(max);
+
+
